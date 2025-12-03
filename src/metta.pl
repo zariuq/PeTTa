802c5ea907485ee1b82af7434c0878bd54e98303
Zar Goertzel <zariuq@gmail.com>
2025-12-03 21:50:06 +0100

    fixed logic bug

--- src/metta.pl
+++ src/metta.pl
@@ -28,16 +28,16 @@ repra(Term, R) :- term_to_atom(Term, R).
 '*'(A,B,R)  :- R is A * B.
 '/'(A,B,R)  :- R is A / B.
 '%'(A,B,R)  :- R is A mod B.
-'<'(A,B,R)  :- (A<B -> R=true ; R=false).
-'>'(A,B,R)  :- (A>B -> R=true ; R=false).
+'<'(A,B,R)  :- (number(A), number(B) -> (A<B -> R=true ; R=false) ; fail).
+'>'(A,B,R)  :- (number(A), number(B) -> (A>B -> R=true ; R=false) ; fail).
 '=='(A,B,R) :- (A==B -> R=true ; R=false).
 '!='(A,B,R) :- (A==B -> R=false ; R=true).
 '='(A,B,R) :-  (A=B -> R=true ; R=false).
 '=?'(A,B,R) :- (\+ \+ A=B -> R=true ; R=false).
 '=alpha'(A,B,R) :- (A =@= B -> R=true ; R=false).
 '=@='(A,B,R) :- (A =@= B -> R=true ; R=false).
-'<='(A,B,R) :- (A =< B -> R=true ; R=false).
-'>='(A,B,R) :- (A >= B -> R=true ; R=false).
+'<='(A,B,R) :- (number(A), number(B) -> (A =< B -> R=true ; R=false) ; fail).
+'>='(A,B,R) :- (number(A), number(B) -> (A >= B -> R=true ; R=false) ; fail).
 % Lexicographic (alphabetic) comparison for strings/atoms
 '@<'(A,B,R) :- (A @< B -> R=true ; R=false).
 '@>'(A,B,R) :- (A @> B -> R=true ; R=false).
